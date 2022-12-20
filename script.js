6f82b06da7fe42f25c34fcdc3c414666e3a1e4f7
joaoabc <112720026+joaoabc@users.noreply.github.com>
2022-12-20 11:16:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+ var nome="RanbirKarbon";
+ console.log (nome)
+
+ var exponential1 = 
+Math.pow(15,1);
+console.log(exponential1);
+
+ var raizquadrada=Math.sqrt(25);
+console.log(raizquadrada);
+
+var exponential1 = 
+Math.pow(50,1);
+console.log(exponential1);
+
+var exponential1 = 
+Math.pow(2,1);
+console.log(exponential1);
